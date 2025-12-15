416fbbf266d63dfe8b664140f960c440f0baa589
waldolflo <waldolf@lebusmagique.fr>
2025-12-15 18:08:21 +0100

    Test 3 logos

--- src/Home.jsx
+++ src/Home.jsx
@@ -231,7 +231,7 @@ export default function Home({ user }) {
     <div className="max-w-7xl mx-auto px-4 py-8">
       {/* LOGO + Titre */}
       <header className="text-center mb-12">
-        <img src="/logo_loidc_Noir_250.png" alt="Logo" className="mx-auto h-28" />
+        <img src="/logo_loidc_Noir_250.png" alt="Logo" className="mx-auto h-28" /><img src="/logo_loidc_Complet_250.png" alt="Logo" className="mx-auto h-28" /><img src="/logo_loidc.png" alt="Logo" className="mx-auto h-28" />
         <h1 className="text-4xl font-bold mt-4">La Loi des Cartes</h1>
         <p className="text-gray-700 mt-2">Découvrez nos jeux, rejoignez notre association pour des après-midi et soirées jeux.</p>
       </header>
