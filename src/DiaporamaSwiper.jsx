e5d8c2dfbcbde5cccd1fddcf7630ee452e40c8b9
waldolflo <waldolf@lebusmagique.fr>
2025-12-21 00:18:53 +0100

    Ajout react

--- src/DiaporamaSwiper.jsx
+++ src/DiaporamaSwiper.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { supabase } from "./supabaseClient";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, EffectFade } from "swiper/modules";
