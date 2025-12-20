c887c96e9ed95cfd985a3654342306cf90d76f9c
waldolflo <waldolf@lebusmagique.fr>
2025-12-20 11:49:56 +0100

    Ajout link dans profils

--- src/Profils.jsx
+++ src/Profils.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom"; 
 import { supabase } from "./supabaseClient";
 import { Navigate } from "react-router-dom";
 
